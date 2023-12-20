75a32b7ef4156362de5dfb3eb3c0757be8524a3a
ai22m030 <ai22m030@technikum-wien.at>
2023-12-20 17:10:53 +0100

    update saving

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ client = MongoClient('localhost', 27017)
 db = client.HistoricalStocks
 collection = db.StockPrices
 
+collection.create_index([("symbol", 1), ("date", 1)], unique=True)
+
 # API details
-api_key = 'T5CHYLWGF8OCKHU5'  # Replace with your actual Alpha Vantage API key
+# api_key = 'T5CHYLWGF8OCKHU5'  # Replace with your actual Alpha Vantage API key
+# api_key = 'OBMOO1ZDW1JZRCW4'
+api_key = 'MZAM9Z5XAQCDHDUP'
 base_url = "https://www.alphavantage.co/query"
 
 # Stock symbols
@@ -42,15 +46,25 @@ def fetch_and_store_data(symbol, month):
         # Assuming 'Time Series (60min)' is the key in the JSON response
         time_series = data.get("Time Series (60min)", {})
         for date, stock_data in time_series.items():
-            stock_data['symbol'] = symbol
-            stock_data['date'] = date.to_pydatetime()
-            collection.insert_one(stock_data)
+            tmp_data = dict()
+            tmp_data['open'] = float(stock_data['1. open'])
+            tmp_data['high'] = float(stock_data['2. high'])
+            tmp_data['low'] = float(stock_data['3. low'])
+            tmp_data['close'] = float(stock_data['4. close'])
+            tmp_data['volume'] = int(stock_data['5. volume'])
+            tmp_data['symbol'] = symbol
+            tmp_data['date'] = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+            collection.update_one(
+                {"symbol": symbol, "date": tmp_data['date']},
+                {"$set": tmp_data},
+                upsert=True
+            )
     else:
         print(f"Error fetching data for {symbol} for the month {month}")
 
 
 if __name__ == '__main__':
-    start_date = datetime(2000, 1, 1)
+    start_date = datetime(2020, 1, 1)
     end_date = datetime.now()
     current_date = start_date
 
