2a1e984cb9b1a24d89ce33ec70beb3797626b3b2
ai22m030 <ai22m030@technikum-wien.at>
2024-01-10 01:23:52 +0100

    connection to internal database and setup config

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import requests
 from pymongo import MongoClient
 from datetime import datetime, timedelta
+from urllib.parse import quote_plus
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 # MongoDB setup
-client = MongoClient('localhost', 27017)
-db = client.HistoricalStocks
+uri = "mongodb://%s:%s@%s:%s/%s" % (quote_plus(config['MongoDB']['user']),
+                                    quote_plus(config['MongoDB']['password']),
+                                    config['MongoDB']['host'],
+                                    config['MongoDB']['port'],
+                                    config['MongoDB']['database'])
+client = MongoClient(uri)
+db = client.StockData
 collection = db.StockPrices
-
 collection.create_index([("symbol", 1), ("date", 1)], unique=True)
 
 # API details
-# api_key = 'T5CHYLWGF8OCKHU5'  # Replace with your actual Alpha Vantage API key
-# api_key = 'OBMOO1ZDW1JZRCW4'
-api_key = 'MZAM9Z5XAQCDHDUP'
-base_url = "https://www.alphavantage.co/query"
+api_key = config['API']['key']
+base_url = config['API']['host']
 
 # Stock symbols
-symbols = [
+symbols_stocks = [
     'AAPL', 'MSFT', 'CSCO', 'WMT', 'INTC', 'PG', 'JNJ', 'KO', 'PEP', 'MCD',
     'MO', 'BA', 'XOM', 'CVX', 'GE', 'CAT', 'MMM', 'HPQ', 'DD', 'MRK',
     'JPM', 'AXP', 'BAC', 'C', 'WFC', 'GS', 'VZ', 'T', 'IBM', 'TXN',
@@ -29,22 +36,49 @@ symbols = [
     'OXY', 'DVN', 'MRO', 'SWN', 'RRC', 'CVS', 'EQT', 'FTI'
 ]
 
+symbols_currency = ["EUR", "GBP", "JPY", "CHF", "CAD"]
 
-def fetch_and_store_data(symbol, month):
-    params = {
-        "function": "TIME_SERIES_INTRADAY",
-        "symbol": symbol,
-        "interval": "60min",
-        "month": month,
-        "outputsize": "full",
-        "apikey": api_key
-    }
+
+def fetch_and_store_data(symbol, month, interval="1min", fetch_currency=False):
+    # Check if data for this symbol and month already exists in the database
+    if fetch_currency:
+        from_to = symbol.split("-")
+
+    if collection.count_documents({"symbol": symbol, "date": {"$gte": datetime.strptime(month, '%Y-%m'),
+                                                              "$lt": datetime.strptime(month, '%Y-%m') + timedelta(
+                                                                      days=31)}}):
+        print(f"Data for {symbol} for the month {month} already exists. Skipping API call.")
+        return
+
+    if fetch_currency:
+        params = {
+            "function": "CURRENCY_EXCHANGE_RATE",
+            "from_symbol": from_to[0],
+            "to_symbol": from_to[1],
+            "interval": interval,  # Assuming you want 60min interval data
+            "outputsize": "full",
+            "apikey": api_key
+        }
+    else:
+        params = {
+            "function": "TIME_SERIES_INTRADAY",
+            "symbol": symbol,
+            "interval": interval,  # Assuming you want 60min interval data
+            "month": month,
+            "outputsize": "full",
+            "apikey": api_key
+        }
 
     response = requests.get(base_url, params=params)
+
     if response.status_code == 200:
         data = response.json()
-        # Assuming 'Time Series (60min)' is the key in the JSON response
-        time_series = data.get("Time Series (60min)", {})
+
+        if "Information" in data:
+            print(f"Error fetching data for {symbol} for the month {month}")
+            return
+
+        time_series = data.get(f"Time Series ({interval})", {})
         for date, stock_data in time_series.items():
             tmp_data = dict()
             tmp_data['open'] = float(stock_data['1. open'])
@@ -70,6 +104,12 @@ if __name__ == '__main__':
 
     while current_date <= end_date:
         month = current_date.strftime('%Y-%m')
-        for symbol in symbols:
+
+        print(f"Fetching data for the month {month}")
+
+        for symbol in symbols_stocks:
+            print(f"Fetching data for {symbol}")
             fetch_and_store_data(symbol, month)
+
+        # Add a month to the current date
         current_date += timedelta(days=31)
